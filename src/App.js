5a087b00714f77d3c43bd7ea7cbb464312e2de83
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-17 13:09:16 -0700

    Developed components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Figure from './components/Figure';
+import Header from './components/Header';
+import WrongLetters from './components/WrongLetters';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+      </div>
+      
     </div>
   );
 }
